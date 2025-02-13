4a4753babfe6b40cbe126bbcce0fdabad920d58a
Zen Path <zen.path@tuta.com>
2025-02-13 21:39:58 +0200

    feat: dynamically generate players

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
-import { Team, TeamBanner, DEFAULT_TEAMS } from "./teams.js";
-
 import { getUniqueRandomElements } from "./utils.js";
+import { Team, TeamBanner, DEFAULT_TEAMS } from "./teams.js";
+import { DEFAULT_PLAYERS } from "./players.js";
 
-const BANNER_CONTAINER = document.body.querySelector("#banner-container");
+const BANNER_CONTAINER = document.getElementById(TeamBanner.BANNER_CONTAINER_ID);
+const SIDEBAR_CONTAINER_LEFT = document.body.querySelector(".sidebar-container.left");
+const SIDEBAR_CONTAINER_RIGHT = document.body.querySelector(".sidebar-container.right");
+
+function generatePlayers(teams, countA, countB, teamBanner) {
+    const players = getUniqueRandomElements(DEFAULT_PLAYERS, countA + countB);
+    teams[0].players = players.slice(0, countA);
+    teams[1].players = players.slice(countA);
+
+    const leftSidebarElem = teams[0].createSidebar();
+    while (SIDEBAR_CONTAINER_LEFT.children[0]) {
+        SIDEBAR_CONTAINER_LEFT.removeChild(SIDEBAR_CONTAINER_LEFT.children[0]);
+    }
+    SIDEBAR_CONTAINER_LEFT.appendChild(leftSidebarElem);
+
+    const rightSidebarElem = teams[1].createSidebar();
+    while (SIDEBAR_CONTAINER_RIGHT.children[0]) {
+        SIDEBAR_CONTAINER_RIGHT.removeChild(SIDEBAR_CONTAINER_RIGHT.children[0]);
+    }
+    SIDEBAR_CONTAINER_RIGHT.appendChild(rightSidebarElem);
+
+    players.forEach((player) => {
+        console.log(player.fullCardElem);
+        player.fullCardElem.addEventListener("click", () => {
+            teamBanner.updateSuccessRate();
+        });
+    });
+}
 
 function main() {
     const teams = getUniqueRandomElements(DEFAULT_TEAMS, 2);
@@ -11,11 +38,32 @@ function main() {
 
     const teamBanner = new TeamBanner(teams);
     const teamBannerElem = teamBanner.create();
-
-    console.log(teams, teamBannerElem, "yo");
     BANNER_CONTAINER.appendChild(teamBannerElem);
 
-    // console.log(BASIC_TEAMS)
+    generatePlayers(teams, 5, 5, teamBanner);
+
+    document.getElementById("teamForm").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const teamData = {
+            teamA: {
+                playerCount: parseInt(formData.get("teamA_player_count")),
+                order: formData.get("teamA_order"),
+            },
+            teamB: {
+                playerCount: parseInt(formData.get("teamB_player_count")),
+                order: formData.get("teamB_order"),
+            },
+        };
+
+        generatePlayers(
+            teams,
+            teamData.teamA.playerCount,
+            teamData.teamB.playerCount,
+            teamBanner,
+        );
+    });
 }
 
 window.onload = () => {
--- js/players.js
+++ js/players.js
@@ -1,3 +1,5 @@
+import { PLAYERS_DATA } from "../data/players.js";
+
 export class Player {
     constructor(firstName, lastName, profilePicturePath, stats = null) {
         this.firstName = firstName;
@@ -8,6 +10,51 @@ export class Player {
 
         this.stats = stats ? stats : new PlayerStats();
         this.stats.randomize();
+
+        this.fullCardElem = null;
+    }
+
+    createFullCard() {
+        // TODO: Implement using proper elements, not innerHTML.
+        const cardElem = document.createElement("div");
+        cardElem.classList.add("full-card");
+        cardElem.innerHTML = `
+            <div class="profile-picture-container">
+                <img
+                class="profile-picture"
+                src="${this.profilePicturePath}"
+                alt="${this.fullName}"
+                draggable="false">
+            </div>
+
+            <p class="first-name">${this.firstName}</p>
+            <p class="last-name">${this.lastName}</p>
+
+            <div class="stats-container">
+                <div class="stats-descriptions">
+                    ${Object.keys(this.stats)
+                        .map(
+                            (description) => `
+                        <p class="description">${description}</p>
+                    `,
+                        )
+                        .join("\n")}
+                </div>
+
+                <div class="stats-values">
+                    ${Object.values(this.stats)
+                        .map(
+                            (value) => `
+                        <p class="value">${String(value).padStart(2, 0)}</p>
+                    `,
+                        )
+                        .join("\n")}
+                </div>
+            </div>
+        `;
+
+        this.fullCardElem = cardElem;
+        return cardElem;
     }
 }
 
@@ -27,63 +74,12 @@ export class PlayerStats {
     }
 }
 
-export const DEFAULT_PLAYERS = [
-    new Player("Lionel", "Messi", "../assets/images/players/lionel-messi.png"),
-    new Player("Cristiano", "Ronaldo", "../assets/images/players/cristiano-ronaldo.png"),
-    new Player("Mohamed", "Salah", "../assets/images/players/mohamed-salah.png"),
-    new Player("Omar", "Marmoush", "../assets/images/players/omar-marmoush.png"),
-    new Player("Jamal", "Musiala", "../assets/images/players/jamal-musiala.png"),
-    new Player("Florian", "Wirtz", "../assets/images/players/florian-wirtz.png"),
-    new Player("Bukayo", "Saka", "../assets/images/players/bukayo-saka.png"),
-    new Player("Vinícius", "Júnior", "../assets/images/players/vinícius-júnior.png"),
-    new Player("Alexander", "Isak", "../assets/images/players/alexander-isak.png"),
-    new Player("Joshua", "Kimmich", "../assets/images/players/joshua-kimmich.png"),
-    new Player("Dayot", "Upamecano", "../assets/images/players/dayot-upamecano.png"),
-    new Player("Achraf", "Hakimi", "../assets/images/players/achraf-hakimi.png"),
-    new Player("Jude", "Bellingham", "../assets/images/players/jude-bellingham.png"),
-    new Player("Kim", "Minjae", "../assets/images/players/kim-minjae.png"),
-    new Player("Ousmane", "Dembélé", "../assets/images/players/ousmane-dembélé.png"),
-    new Player("Kylian", "Mbappé", "../assets/images/players/kylian-mbappé.png"),
-    new Player(
-        "Robert",
-        "Lewandowski",
-        "../assets/images/players/robert-lewandowski.png",
-    ),
-    new Player("Cole", "Palmer", "../assets/images/players/cole-palmer.png"),
-    new Player("Harry", "Kane", "../assets/images/players/harry-kane.png"),
-    new Player("Antoine", "Griezmann", "../assets/images/players/antoine-griezmann.png"),
-    new Player("Michael", "Olise", "../assets/images/players/michael-olise.png"),
-    new Player("Bryan", "Mbeumo", "../assets/images/players/bryan-mbeumo.png"),
-    new Player(
-        "Trent",
-        "Alexander-Arnold",
-        "../assets/images/players/trent-alexander-arnold.png",
-    ),
-    new Player("Toni", "Kroos", "../assets/images/players/toni-kroos.png"),
-    new Player("Matheus", "Cunha", "../assets/images/players/matheus-cunha.png"),
-    new Player("Jordan", "Pickford", "../assets/images/players/jordan-pickford.png"),
-    new Player("Julián", "Álvarez", "../assets/images/players/julián-álvarez.png"),
-    new Player("Íñigo", "Martínez", "../assets/images/players/íñigo-martínez.png"),
-    new Player("Ayoze", "Pérez", "../assets/images/players/ayoze-pérez.png"),
-    new Player("Bradley", "Barcola", "../assets/images/players/bradley-barcola.png"),
-    new Player("Amir", "Rrahmani", "../assets/images/players/amir-rrahmani.png"),
-    new Player(
-        "Vanja",
-        "Milinković-Savić",
-        "../assets/images/players/vanja-milinković-savić.png",
-    ),
-    new Player("Tim", "Kleindienst", "../assets/images/players/tim-kleindienst.png"),
-    new Player("Angelo", "Stiller", "../assets/images/players/angelo-stiller.png"),
-    new Player("Yann", "Sommer", "../assets/images/players/yann-sommer.png"),
-    new Player("Declan", "Rice", "../assets/images/players/declan-rice.png"),
-    new Player("William", "Saliba", "../assets/images/players/william-saliba.png"),
-    new Player("Exequiel", "Palacios", "../assets/images/players/exequiel-palacios.png"),
-    new Player("Erling", "Haaland", "../assets/images/players/erling-haaland.png"),
-    new Player("Phil", "Foden", "../assets/images/players/phil-foden.png"),
-    new Player("Daniel", "Vivian", "../assets/images/players/daniel-vivian.png"),
-    new Player("Maxence", "Lacroix", "../assets/images/players/maxence-lacroix.png"),
-    new Player("Mateo", "Retegui", "../assets/images/players/mateo-retegui.png"),
-    new Player("Cody", "Gakpo", "../assets/images/players/cody-gakpo.png"),
-    new Player("Alphonso", "Davies", "../assets/images/players/alphonso-davies.png"),
-    new Player("Ademola", "Lookman", "../assets/images/players/ademola-lookman.png"),
-];
+const PLAYER_IMAGES_DIR = "../assets/images/players";
+
+export const DEFAULT_PLAYERS = PLAYERS_DATA.map((player) => {
+    return new Player(
+        player.firstName,
+        player.lastName,
+        `${PLAYER_IMAGES_DIR}/${player.imageName}`,
+    );
+});
