1850fd4099a18ec90f88d0615586a2cd8806fcdc
Zen Path <zen.path@tuta.com>
2025-02-18 18:45:42 +0200

    feat: add team and team logo class

---
+++ js/team.js
@@ -0,0 +1,21 @@
+class TeamLogo {
+    DEFAULT_LOGO_DIR = "../assets/images/teams/";
+
+    constructor(fileName, src = null) {
+        this.path = this.DEFAULT_LOGO_DIR + fileName;
+        this.src = src;
+    }
+}
+
+class Team {
+    COLORS = {
+        RED: "#d9042b",
+        BLUE: "#03658c",
+    };
+
+    constructor(name, logo, color = null) {
+        this.name = name;
+        this.logo = logo;
+        this.color = color;
+    }
+}
